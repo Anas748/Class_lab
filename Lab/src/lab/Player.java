4634ea97703dbe9f62a6cebb7fc14543f04c9c5a
Anas748 <100359811+Anas748@users.noreply.github.com>
2023-12-15 13:30:29 +0000

    every time a user will run it will initailze them to empty and return null

--- Lab/src/lab/Player.java
+++ Lab/src/lab/Player.java
@@ -70,48 +70,55 @@ public class Player {
     public void setBackground(String background) {
         this.background = background;
     }
-    public static Player createPlayer(){
-         System.out.println("Please enter the player's name: ");
-                    name = sc.nextLine();
-                    System.out.println("Please enter the player's number: ");
-                    do {
-                        try {
-                            number = Integer.parseInt(sc.nextLine()); 
-                            if (number < 1) {
-                                System.out.println("Please enter a positive integer");
-                            } else validPlayer = true;
+    public static Player createPlayer() {
+        String name;
+        int number = 0;
+        String birth;
+        String position;
+        int goalsScored = 0;
+        String background;
+        System.out.println("Please enter the player's name: ");
+        name = sc.nextLine();
+        System.out.println("Please enter the player's number: ");
+        do {
+            try {
+                number = Integer.parseInt(sc.nextLine());
+                if (number < 1) {
+                    System.out.println("Please enter a positive integer");
+                } else {
+                    validPlayer = true;
+                }
 
-                        } catch (Exception e) {
-                            System.out.println("That is not a number. please try again!");
-                        }
-                    } while (!validPlayer);                                              
-                    System.out.println("Please enter the player's date of birth: ");
-                    birth = sc.nextLine();         
-                    System.out.println("Please enter the player's position: ");
-                    position = sc.nextLine(); 
-                    System.out.println("Please enter the number of goals the player has scored: ");
-                    validPlayer = false;
-                    do {
-                        try {
-                            goalsScored = Integer.parseInt(sc.nextLine()); 
-                            if (goalsScored < 1) {
-                                System.out.println("Please enter a positive integer");
-                            } else validPlayer = true;
+            } catch (Exception e) {
+                System.out.println("That is not a number. please try again!");
+            }
+        } while (!validPlayer);
+        System.out.println("Please enter the player's date of birth: ");
+        birth = sc.nextLine();
+        System.out.println("Please enter the player's position: ");
+        position = sc.nextLine();
+        System.out.println("Please enter the number of goals the player has scored: ");
+        validPlayer = false;
+        do {
+            try {
+                goalsScored = Integer.parseInt(sc.nextLine());
+                if (goalsScored < 1) {
+                    System.out.println("Please enter a positive integer");
+                } else {
+                    validPlayer = true;
+                }
 
-                        } catch (Exception e) {
-                            System.out.println("That is not a number. please try again!");
-                        }while (!validPlayer);     
-                    System.out.println("Please enter the player's background: ");
-                    background = sc.nextLine();                          
-                    System.out.println("Thank you for entering a player"); 
-        
-        
-        
-        return null;
-        
-    
+            } catch (Exception e) {
+                System.out.println("That is not a number. please try again!");
+            }
+            while (!validPlayer);
+            System.out.println("Please enter the player's background: ");
+            background = sc.nextLine();
+            System.out.println("Thank you for entering a player");
 
-}
-}
+             return new Player(name, number, birth, position, goalsScored, background);
+
+        }
+    }
 }
    
