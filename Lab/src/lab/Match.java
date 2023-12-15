b1a6320a272a9a74d1c0044d8bf56810ef2d59e1
Haider007A <100359869+Haider007A@users.noreply.github.com>
2023-12-15 13:02:16 +0000

    Started variables for team1,2 and add getters

--- Lab/src/lab/Match.java
+++ Lab/src/lab/Match.java
@@ -9,5 +9,35 @@ package lab;
  * @author PC
  */
 public class Match {
+    // team 1 an 2 represents the two teams
+     private Team team1;
+    private Team team2;
+  //  team1 and 2 score int represents the score of team
+    private int team1Score;
+    private int team2Score;
     
+    
+    // contructor to initialize the team1,2 variables
+     public Match(Team team1, Team team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+        
+    }
+     
+     //Geters methods for team1,2
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public int getTeam1Score() {
+        return team1Score;
+    }
+
+    public int getTeam2Score() {
+        return team2Score;
+    }
 }
