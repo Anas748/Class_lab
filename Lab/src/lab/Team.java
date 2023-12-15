e49f4fa2984de828ba6ac56798fde5d06c7baf83
Anas748 <100359811+Anas748@users.noreply.github.com>
2023-12-15 13:42:54 +0000

    for teams

--- Lab/src/lab/Team.java
+++ Lab/src/lab/Team.java
@@ -4,14 +4,35 @@
  */
 package lab;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author PC
  */
 class Team {
 
-    String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        private String name;
+    private ArrayList<Player> players;
+
+    public Team(String name) {
+        this.name = name;
+        this.players = new ArrayList<>();
+      
+        
+    }
+  
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
     }
     
 }
